69f89d15b9a81fb3b4555a91d4aaafef4d3ab6bf
mobubai <mbb1229@126.com>
2017-11-11 22:33:36 +0800

    调整

---
+++ backup/timeline/compiled/examples/groups_test_2json.php
@@ -0,0 +1,278 @@
+{
+    "scale": "human",
+	"title": {
+		        "media": {
+		            "caption": "",
+		            "credit": "",
+		            "url": "IMG_1597.jpg",
+		            "thumb": 	""
+		        },
+	            "text": {
+	                "headline": "Welcome to Kiki's Creative World",
+	                "text": "<p></br>Wish you have get ready to explore more about me.</br> Click on the arrow to the right to learn more.</p>"
+	            }
+	    },
+    "events": [
+        {
+            "start_date": {
+                "year":			"2013",
+                "month":		"9",
+                "day": 			"",
+                "hour": 		"",
+                "minute": 		"",
+                "second": 		"",
+                "millisecond": 	"",
+                "format": 		""
+            },
+            "end_date": {
+                "year":         "2017",
+                "month":        "6",
+                "day":          "",
+                "hour":         "",
+                "minute":       "",
+                "second":       "",
+                "millisecond":  "",
+                "format":       ""
+            },
+			"group": "Education",
+            "media": {
+                "caption": "Shanghai International Studies University",
+                "credit": "",
+                "url": "https://youtu.be/9iHf7yVM8Uk",
+                "thumb": 	""
+            },
+            "text": {
+                "headline": "Shanghai International Studies University",
+                "text": "</br>I used to study International Journalism in SISU. I learned jourmalism theories and practice, multi-media production and English languages there. I appreciate SISU a lot not only for the knowledge I have learned, but also the precious experience and people there. SISU is a university with open atmosphere where I learned to think independently and differently."
+            }
+        },
+        {
+            "start_date": {
+                "year":			"2016",
+                "month":		"6",
+                "day": 			"",
+                "hour": 		"",
+                "minute": 		"",
+                "second": 		"",
+                "millisecond": 	"",
+                "format": 		""
+            },
+            "end_date": {
+                "year":         "2017",
+                "month":        "2",
+                "day":          "",
+                "hour":         "",
+                "minute":       "",
+                "second":       "",
+                "millisecond":  "",
+                "format":       ""
+            },
+			"group": "Internship",
+            "media": {
+                "caption": "",
+                "credit": "",
+                "url": "elle.jpg",
+                "thumb": 	""
+            },
+            "text": {
+                "headline": "ELLE CHINA",
+                "text": "<br>△ Responsible for daily updating of the column Elle China lifestyle<br>△ Contribute to ELLEChina WeChat, with a daily average page view at about 20 000, reaching a peak at over 70 000 and 400 likes<br>△ Attend PR events as media representative"
+            }
+        },
+        {
+            "start_date": {
+                "year":			"2014",
+                "month":		"9",
+                "day": 			"",
+                "hour": 		"",
+                "minute": 		"",
+                "second": 		"",
+                "millisecond": 	"",
+                "format": 		""
+            },
+            "end_date": {
+                "year":         "2017",
+                "month":        "6",
+                "day":          "",
+                "hour":         "",
+                "minute":       "",
+                "second":       "",
+                "millisecond":  "",
+                "format":       ""
+            },
+			"group": "Practice",
+            "media": {
+                "caption": "SISUSNAP",
+                "credit": "",
+                "url": "sisusnap.gif",
+                "thumb": 	""
+            },
+            "text": {
+                "headline": "SISUSNAP",
+                "text": "</br>If you are not creating, you are dying. SISUSNAP is a campus organization focusing on personality, trend, and fashion in SISU. The main events of this organization are weekly campus street snap, digital magazine SUSHI, and theme activities. And i'm in responsible for:</br></br>△ Discussing SUSHI topics</br>△ Managing official WeChat account of SISUSNAP, responsible for the regular update and online follow-up activities</br>△ Responsible for copywriting of theme activities"
+            }
+        },
+        {
+            "start_date": {
+                "year":			"2015",
+                "month":		"7",
+                "day": 			"",
+                "hour": 		"",
+                "minute": 		"",
+                "second": 		"",
+                "millisecond": 	"",
+                "format": 		""
+            },
+            "end_date": {
+                "year":         "2015",
+                "month":        "9",
+                "day":          "",
+                "hour":         "",
+                "minute":       "",
+                "second":       "",
+                "millisecond":  "",
+                "format":       ""
+            },
+			"group": "Internship",
+            "media": {
+                "caption": "Life of Pure Pleasure",
+                "credit": "",
+                "url": "purelife.png",
+                "thumb": 	""
+            },
+            "text": {
+                "headline": "Life of Pure Pleasure",
+                "text": "</br>Life of Pure Pleasure is a finance talk show launched by Shanghai YouEra Media Co.,Ltd. And I'm responsible for:</br></br>△ Managing the daily update of the official WeChat account for programme promotion and finance information sharing, enlarging followers from 28587 to 52683 with a daily average page view at about 5000, reaching a peak at over 100 000.</br>△ Participating in show recording, coordinate with guests and deal with emergencies."
+            }
+        },
+        {
+            "start_date": {
+                "year":			"2017",
+                "month":		"9",
+                "day": 			"",
+                "hour": 		"",
+                "minute": 		"",
+                "second": 		"",
+                "millisecond": 	"",
+                "format": 		""
+            },
+			"group": "Education",
+            "media": {
+                "caption": "",
+                "credit": "",
+                "url": "CUHK.jpeg",
+                "thumb": 	""
+            },
+            "text": {
+                "headline": "Chinese University of Hong Kong",
+                "text": " </br>In the media study, I have the chance to communicate with quite a lot of people like the famous artists, musicians, and even the Yoga teacher through both new media and traditional media, from which I got to be aware that the two channels are closely related, and which inspired me to pursue higher study in New Media in CUHK where numbers of prestigious guest speakers bring the cutting edge cultural information. In CUHK, I'm glad to be exposed to a more dynamic environment and have the chance to broaden my horizen."
+            }
+        },
+        {
+            "start_date": {
+                "year":			"2013",
+                "month":		"9",
+                "day": 			"",
+                "hour": 		"",
+                "minute": 		"",
+                "second": 		"",
+                "millisecond": 	"",
+                "format": 		""
+            },
+            "end_date": {
+                "year":			"2017",
+                "month":		"6",
+                "day": 			"",
+                "hour": 		"",
+                "minute": 		"",
+                "second": 		"",
+                "millisecond": 	"",
+                "format": 		""
+            },
+			"group": "Practice",
+            "media": {
+                "caption": "SJC Basketball Team",
+                "credit": "",
+                "url": "basketball.jpeg",
+                "thumb": 	""
+            },
+            "text": {
+                "headline": "SJC Basketball Team",
+                "text": "</br>The four years I spent in basketball team of School of Journalism and Communication as a team manager is previous where I really learn the meaning of team. We have the cohesion power and the sense of group honor. It is perfect to strive for one common goal together. Even what I do in this team is just some trival things like logistics and stroke analysis, I have always tried my best to serve the whole team and care about every member."
+            }
+        },
+
+        {
+            "start_date": {
+                "year":			"2016",
+                "month":		"9",
+                "day": 			"",
+                "hour": 		"",
+                "minute": 		"",
+                "second": 		"",
+                "millisecond": 	"",
+                "format": 		""
+            },
+			"group": "Internship",
+            "media": {
+                "caption": "",
+                "credit": "Shen Jie, Senior Feature Editor from ELLE China",
+                "url": "<blockquote><p>Kiki is responsible, careful and creative. She is a rare talent and I'd like to work with her.</p></blockquote>"
+            },
+            "text": {
+                "headline": "Remarks On My Work",
+                "text": "These are remarks from my previous colleagues."
+            },
+
+            "unique_id": ""
+        },
+          {
+            "start_date": {
+                "year":         "2017",
+                "month":        "2",
+                "day":          "",
+                "hour":         "",
+                "minute":       "",
+                "second":       "",
+                "millisecond":  "",
+                "format":       ""
+            },
+            
+            "group": "Internship",
+            "media": {
+                "caption": "",
+                "credit": "Emin Luo, Feature Editor from ELLE China",
+                "url": "<blockquote><p>Kiki is always good at catching the key point. She has mature writing skills as well as own special ideas, which always brings me surprises. I cannot help thinkng anout her whenever I have a new intern.</p></blockquote>"
+            },
+            "text": {
+                "headline": "Remarks On My Work",
+                "text": ""
+            },
+            "unique_id": ""
+        },
+         {
+            "background": {
+                "color": "#b09b9b",
+                "opacity":50 ,
+                "url": null
+            },
+            "start_date": {
+                "year":         "2018",
+                "month":        "",
+                "day":          "",
+                "hour":         "",
+                "minute":       "",
+                "second":       "",
+                "millisecond":  "",
+                "thumbnail":    "",
+                "format":       ""
+            },
+            "text": {
+                "headline": "To Be Continued",
+                "text": ""
+            },
+            "unique_id": ""
+        }
+    
+    ]
+}
--- timeline/compiled/examples/groups_test_2json.php
+++ timeline/compiled/examples/groups_test_2json.php
@@ -4,7 +4,7 @@
 		        "media": {
 		            "caption": "",
 		            "credit": "",
-		            "url": "IMG_1597.jpg",
+		            "url": "IMG_1597.JPG",
 		            "thumb": 	""
 		        },
 	            "text": {
@@ -67,18 +67,19 @@
                 "millisecond":  "",
                 "format":       ""
             },
-			"group": "Internship",
+            "group": "Internship",
             "media": {
-                "caption": "",
+                "caption": "ELLE CHINA",
                 "credit": "",
                 "url": "elle.jpg",
-                "thumb": 	""
+                "thumb":    ""
             },
             "text": {
                 "headline": "ELLE CHINA",
                 "text": "<br>△ Responsible for daily updating of the column Elle China lifestyle<br>△ Contribute to ELLEChina WeChat, with a daily average page view at about 20 000, reaching a peak at over 70 000 and 400 likes<br>△ Attend PR events as media representative"
             }
         },
+			
         {
             "start_date": {
                 "year":			"2014",
@@ -104,7 +105,7 @@
             "media": {
                 "caption": "SISUSNAP",
                 "credit": "",
-                "url": "sisusnap.gif",
+                "url": "http://mmbiz.qpic.cn/mmbiz_gif/lePQSD9cvUGibTZzxe1AoAPk5w7miburCAv6QbD8QftANBotUMQzOsCibkVgYibnyAxpw0ibA4VfYJPfzbibby1InoSA/0?wx_fmt=gif&tp=webp&wxfrom=5&wx_lazy=1",
                 "thumb": 	""
             },
             "text": {
@@ -137,7 +138,7 @@
             "media": {
                 "caption": "Life of Pure Pleasure",
                 "credit": "",
-                "url": "purelife.png",
+                "url": "http://img0.ph.126.net/A_TSNjavLQJ56cR7UNG5dA==/2599139934965322219.png",
                 "thumb": 	""
             },
             "text": {
@@ -160,7 +161,7 @@
             "media": {
                 "caption": "",
                 "credit": "",
-                "url": "CUHK.jpeg",
+                "url": "http://img1.ph.126.net/Cl3U9lG7CidWuZ29qBlCWQ==/6632753317025039902.jpeg",
                 "thumb": 	""
             },
             "text": {
@@ -193,7 +194,7 @@
             "media": {
                 "caption": "SJC Basketball Team",
                 "credit": "",
-                "url": "basketball.jpeg",
+                "url": "http://img2.ph.126.net/Ti6vg0QwtvnGO5L-pLba1w==/6632647763908765115.jpeg",
                 "thumb": 	""
             },
             "text": {
